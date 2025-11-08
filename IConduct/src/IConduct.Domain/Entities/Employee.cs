26f4b4f815cf14c33565e9f06e96568ffd1b2818
Олег Думанский <o.dumanskiy@avto.pro>
2025-11-08 16:28:59 +0200

    Implemented domain structure and references

---
+++ IConduct/src/IConduct.Domain/Entities/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace IConduct.Domain.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int? ManagerId { get; set; }
+        public bool Enable { get; set; }
+        public List<Employee>? Employees { get; set; }
+    }
+}
---
+++ IConduct/src/IConduct.Domain/Interfaces/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,10 @@
+﻿using IConduct.Domain.Entities;
+
+namespace IConduct.Domain.Interfaces.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Employee GetEmployeeById(int id);
+        bool EnableEmployee(int id);
+    }
+}
